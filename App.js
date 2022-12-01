ad58a62fd52f3078281efb7a7f1ab2d56d5fba62
abdullah zareen <abdullahzareen90@gmail.com>
2022-12-01 21:56:48 +0500

    circle slider in react native added

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import React from 'react';
-import ImagePickerScreen from './src/ImagePickerScreen';
+import React, {useState} from 'react';
+
+import CircleSlider from './CircularSlider';
 export default function App() {
+  const [sliderValue, setSliderValue] = useState(1 / 6);
+  console.log('slidervalue', (sliderValue / 6).toFixed());
   return (
-    <View>
-      <ImagePickerScreen />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        //   backgroundColor: 'black',
+      }}>
+      <CircleSlider
+        value={sliderValue}
+        max={359}
+        onValueChange={x => x}
+        setSliderValue={setSliderValue}>
+        <Text>{sliderValue}</Text>
+      </CircleSlider>
     </View>
   );
 }
